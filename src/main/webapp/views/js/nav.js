ebbac0623ae14877a0a2d1251f1a43c1b82bd108
Chen Shuo <546537825@qq.com>
2016-06-24 11:21:19 +0800

    Add css js.

---
+++ src/main/webapp/views/js/nav.js
@@ -0,0 +1,34 @@
+$(function() {
+	var Accordion = function(el, multiple) {
+		this.el = el || {};
+		this.multiple = multiple || false;
+
+		// Variables privadas
+		var links = this.el.find('.link');
+		// Evento
+		links.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+	}
+
+	Accordion.prototype.dropdown = function(e) {
+		var $el = e.data.el;
+			$this = $(this),
+			$next = $this.next();
+
+		$next.slideToggle();
+		$this.parent().toggleClass('open');
+
+		if (!e.data.multiple) {
+			$el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+		};
+	}
+
+    var accordion = new Accordion($('#accordion'), false);
+
+
+});
+
+function highlightTab(tab_1, tab_2){
+    $(`#${tab_1} .submenu`).slideToggle();
+    $(`#${tab_1}`).toggleClass('open');
+    $(`#${tab_2} a`).toggleClass('open');
+}
