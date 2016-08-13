c5dc31721f9109261fc9b110360ccba4c49e8b7d
DengrongGuan12 <1031212913@qq.com>
2016-08-13 12:17:56 +0800

    list

---
+++ src/main/java/controller/PurchaseOrderController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by DengrongGuan on 2016/8/13.
+ */
+@Controller
+@RequestMapping("/v1/purchaseOrderController")
+public class PurchaseOrderController {
+}
